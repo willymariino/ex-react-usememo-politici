18662146959651960729bb5ad6443b499dd79d75
willymariino <wg.mariino@gmail.com>
2025-08-03 11:38:03 +0200

    scaffolding milestone 1

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,29 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+/*
+ Premessa: Stai costruendo una pagina per visualizzare una lista di politici. Tuttavia, vuoi evitare calcoli inutili e ottimizzare la performance del tuo componente. Segui le milestone per migliorare progressivamente il codice
+
+ 📌 Milestone 1: Recuperare e visualizzare i dati
+Effettua una chiamata API a
+http://localhost:3333/politicians
+
+Salva la risposta in uno stato React (useState).
+
+Mostra i politici in una lista di card, visualizzando almeno le seguenti proprietà:
+
+Nome (name)
+Immagine (image)
+Posizione (position)
+Breve biografia (biography)
+
+Obiettivo: Caricare e mostrare i politici in un’interfaccia chiara e leggibile.
+
+*/
 
 function App() {
-  const [count, setCount] = useState(0)
+
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+
     </>
   )
 }
